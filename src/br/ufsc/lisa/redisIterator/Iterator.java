f59f317a32b1c4549ab7e077fa48fef6c8732f4e
Rian <riancvb@gmail.com>
2018-03-13 23:07:59 -0300

    Fixed and building readRedis function.

--- src/br/ufsc/lisa/redisIterator/Iterator.java
+++ src/br/ufsc/lisa/redisIterator/Iterator.java
@@ -1,36 +1,41 @@
 package br.ufsc.lisa.redisIterator;
+
 import java.util.List;
+import java.util.Map.Entry;
+
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import redis.clients.jedis.*;
 
-
 public class Iterator {
-	
+
 	private Jedis jedis;
 
-	public void startServer() { 
-	      Jedis jedis = new Jedis("localhost"); 
-	      System.out.println("Connection to server sucessfully"); 
-	      jedis.close();
-	     }
-	
-	public void checkConnection() {
-		
-		 System.out.println("Server is running: "+jedis.ping()); 
-	   	}
-	
-	public void listAllKeys() {
-	    Jedis jedis = new Jedis("localhost"); 
-		System.out.println("Keys: "+ jedis.keys("*"));
+	public void startServer() {
+		Jedis jedis = new Jedis("localhost");
+		System.out.println("Connection to server sucessfully");
 		jedis.close();
 	}
-	
-	public void push(String key, String value) {
-		Jedis jedis = new Jedis("localhost"); 
-	    jedis.rpush(key, value);
-	    jedis.close(); 
+
+	public void checkConnection() {
+
+		System.out.println("Server is running: " + jedis.ping());
 	}
-	
+
+	public void listAllKeys() {
+		Jedis jedis = new Jedis("localhost");
+		System.out.println("Keys: " + jedis.keys("*"));
+		jedis.close();
+	}
+
+	public void push(String key, String value) {
+		Jedis jedis = new Jedis("localhost");
+		jedis.rpush(key, value);
+		jedis.close();
+	}
+
 	public void delete(String key) {
 		Jedis jedis = new Jedis("localhost"); 
 		jedis.rpop(key);
@@ -38,37 +43,54 @@ public class Iterator {
 	}
 	
 	public void readRedis() {
-		Jedis jedis = new Jedis("localhost"); 
-		if(jedis != null) {
-			ScanParams params = new ScanParams();
-//			params.match("*rian*");
+		Jedis jedis = new Jedis("localhost");
+		
+		ScanResult<String> scanResult = jedis.scan("0");
+		List<String> keys = scanResult.getResult();
+		String nextCursor = scanResult.getStringCursor();
+		JSONParser parser = new JSONParser();
+		int counter = 0;
+		
+		while(true) {
 			
-			
-
-			ScanResult<String> scanResult = jedis.scan("0", params);
-			List<String> keys = scanResult.getResult();
-			String nextCursor = scanResult.getStringCursor();
-			int counter = 0;
-
-			while (true) {
-			    for (String key : keys) {
-//			        createSchema(key);
-			        System.out.println(nextCursor + "nextCursor");
-			        JSONObject jsonObj = new JSONObject();
-			    
-
-			    // An iteration also ends at "0"
-			    if (nextCursor.equals("0")) {
-			        break;
-			    }
-
-			    scanResult = jedis.scan(nextCursor, params);
-			    nextCursor = scanResult.getStringCursor();
-			    keys = scanResult.getResult();
-			    System.out.println(keys = scanResult.getResult());
+			if(nextCursor.equals("0")) {
+				break;
 			}
+			
+			scanResult = jedis.scan(nextCursor);
+			nextCursor = scanResult.getStringCursor();
+			keys = scanResult.getResult();
+			for(counter = 0; counter <= keys.size(); counter++) {
+				try {
+					JSONObject json = (JSONObject) parser.parse(keys.get(counter).toString());
+
+				} catch (ParseException e) {
+					e.printStackTrace();
+				} 
+			}
+			System.out.println(keys = scanResult.getResult());
 		}
 		jedis.close();
-		
+	}
+	
+	public void readRedis2() {
+		Jedis jedis = new Jedis("localhost");
+
+		String key = "THEKEY";
+		ScanParams scanParams = new ScanParams().count(100);
+		String cur = redis.clients.jedis.ScanParams.SCAN_POINTER_START; 
+		boolean cycleIsFinished = false;
+		while(!cycleIsFinished){
+		  ScanResult<Entry<String, String>> scanResult = jedis.scan(key, cur, scanParams);
+		  List<Entry<String, String>> result = scanResult.getResult();
+		  System.out.println(result);
+		  //do whatever with the key-value pairs in result
+
+		  cur = scanResult.getStringCursor();
+		  if (cur.equals("0")){
+		    cycleIsFinished = true;
+		  }                 
+		}
+		jedis.close();
 	}
 }
--- src/br/ufsc/lisa/redisIterator/Main.java
+++ src/br/ufsc/lisa/redisIterator/Main.java
@@ -5,14 +5,15 @@ import br.ufsc.lisa.redisIterator.Iterator;
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		Iterator iterator = new Iterator();
 //		iterator.startServer();
 //		iterator.listAllKeys();
 //		iterator.scanKeys();
 //		iterator.push("rianzz", "sadas");
 //		iterator.delete("rianzera");
-		iterator.readRedis();
+		iterator.readRedis2();
+		
 	}
 
 }
